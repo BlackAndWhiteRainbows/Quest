91949026ec4a14eb388c06ec059dcdb6f3259765
Nicole Creshon <ncreshon@gmail.com>
2018-01-12 15:39:00 -0600

    [setup] fix webpack bug for jsx

--- public/client/index.jsx
+++ public/client/index.jsx
@@ -1,3 +1,17 @@
 console.log('Hey guys and ladies!!');
 console.log('work dang it');
 console.log('hey dude');
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Hello = function (name) {
+  return (
+    <div>Hello, {name}</div>
+  );
+};
+
+const view = Hello("Will");
+
+const element = document.getElementById("root");
+ReactDOM.render(view, element);
---
+++ src/database/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  DB_URI: 'postgres://bjbijjrp:zskru3s6AtaOCXztGGT8k54_zN3Ib2S6@baasu.db.elephantsql.com:5432/bjbijjrp',
+};
